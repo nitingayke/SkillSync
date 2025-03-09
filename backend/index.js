496ac85d048e2ce5c8f73e8950587fe05c978df5
Anil Sonawane <anil.sonawane2003@gmail.com>
2025-03-09 15:00:53 +0530

    Add DB config

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import httpStatus from "http-status";
+import connectDB from "./config/db.js";
+
 
 dotenv.config();
-
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+
+connectDB()
 
 app.get("/", (req, res) => {
     res.status(httpStatus.OK).json({ message: "API is running..." });
